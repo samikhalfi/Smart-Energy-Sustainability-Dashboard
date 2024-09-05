e3c6f6f65417e08b2109d081ce571d3a7d08a84a
Khalfi Sami <121679956+samikhalfi@users.noreply.github.com>
2024-09-05 20:06:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ st.set_page_config(
 
 from authentication.login import login
 from authentication.register import register
-from dashboard import dashboard
+from dashboard.dashboard import dashboard
 
 def main():
 
